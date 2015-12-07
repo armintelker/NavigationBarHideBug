293226eb315db0dbf4f50978a0a19da12e64e49d
Armin Telker <a.telker@rebuy.de>
2015-12-07 13:50:08 +0100

    add navigationbar hide bug

---
+++ NavigationBarHideBug/RootPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace NavigationBarHideBug
+{
+    public partial class RootPage : ContentPage
+    {
+        public RootPage()
+        {
+            InitializeComponent();
+            button.Clicked += async (sender, e) => {
+                await Navigation.PushAsync(new SecondPage());
+            };
+        }
+    }
+}
+
---
+++ NavigationBarHideBug/SecondPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace NavigationBarHideBug
+{
+    public partial class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent();
+
+            button.Clicked += async (object sender, EventArgs e) => {
+                await Navigation.PopAsync();
+            };
+        }
+    }
+}
+
